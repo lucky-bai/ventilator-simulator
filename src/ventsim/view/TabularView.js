61a23fd122ed433f73d47a4ecebc70b2ec93d67c
Isaac Waller <isaac@siriusapplications.com>
2020-04-11 13:11:45 -0700

    Refinements to tabular view

--- src/ventsim/view/TabularView.js
+++ src/ventsim/view/TabularView.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Control from './controls/Control';
 import VariableName from './controls/VariableName';
 
 function TabularRow(props) {
     
-    return <tr>
-        {props.inputs.map((i) => <td key={i}>{props.input ? (props.input[i.key] + (i.unit ? (" " + i.unit) : "")) : null}</td>)}
-        {props.outputs.map((i) => <td key={i}>{props.output ? (props.output[i.key] + (i.unit ? (" " + i.unit) : "")) : null}</td>)}
+    return <tr onClick={() => props.onChangeInput(props.input)} className={props.lastRow ? "current" : ""}>
+        {props.inputs.map((i) => <td key={i.key}>{props.input ? (props.input[i.key] + (i.unit ? (" " + i.unit) : "")) : null}</td>)}
+        {props.outputs.map((i) => <td key={i.key}>{props.output ? (props.output[i.key] + (i.unit ? (" " + i.unit) : "")) : null}</td>)}
     </tr>;
 }
 
@@ -22,34 +22,51 @@ function TabularView(props) {
         let historyEntry = props.history[i];
         let entryOutputs = historyEntry[1];
 
-        rows.push(<TabularRow inputs={inputs} outputs={outputs}
+        rows.push(<TabularRow key={i} inputs={inputs} outputs={outputs}
             input={historyEntry[0]}
-            output={entryOutputs.length > 0 ? entryOutputs[0] : null} />);
+            output={entryOutputs.length > 0 ? entryOutputs[0] : null}
+            onChangeInput={props.onChangeInput}
+            lastRow={i == (props.history.length - 1) && entryOutputs.length < 2} />);
 
         if (entryOutputs.length > 1) {
             for (var j = 1; j < entryOutputs.length; j += 1) {
-                rows.push(<TabularRow inputs={inputs} outputs={outputs} output={entryOutputs[j]} />);
+                rows.push(<TabularRow key={i+"."+j} inputs={inputs} outputs={outputs} output={entryOutputs[j]}
+                    onChangeInput={props.onChangeInput}
+                    lastRow={i == (props.history.length - 1) && j == (entryOutputs.length - 1)} />);
             }
         }
     }
     
+    const tableContainerRef = useRef(null);
+
+    let cellClass = (key) => (key.endsWith("_1") ? "patient-1" : (key.endsWith("_2") ? "patient-2" : ""));
+    
+    const scrollToBottom = () => {
+        tableContainerRef.current.scrollTop = tableContainerRef.current.scrollHeight;
+    }
+
+    useEffect(scrollToBottom);
+    
     return <div className="tabular-view">
         <h1>Log</h1>
-        <table>
-            <thead>
-                <tr><th colspan={inputs.length}>Input</th><th colspan={outputs.length}>Output</th></tr>
-                <tr>
-                    {inputs.map((i) => <th key={i.key}><VariableName variable={i} /></th>)}
-                    {outputs.map((i) => <th key={i.key}><VariableName variable={i} /></th>)}
-                </tr>
-            </thead>
-            <tbody>{rows}</tbody>
-        </table>
+        <div className="table-container" ref={tableContainerRef}>
+            <table>
+                <thead>
+                    <tr><th colSpan={inputs.length}>Input</th><th colSpan={outputs.length}>Output</th></tr>
+                    <tr>
+                        {inputs.map((i) => <th key={i.key} className={cellClass(i.key)}><VariableName variable={i} /></th>)}
+                        {outputs.map((i) => <th key={i.key} className={cellClass(i.key)}><VariableName variable={i} /></th>)}
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+        </div>
+        <p>Click on a row to reset input values to those values. Current values are highlighted in <span className="current">green</span></p>
         <h1>Inputs</h1>
         <div className="control-container tabular-controls">
             {props.inputs.map((input) => {
-                return <Control variable={input} mutable={true} value={currentInput[input.key]} onChange={(e) => {
-                    currentInput[input] = e.target.value;
+                return <Control key={input.key} variable={input} mutable={true} value={currentInput[input.key]} onChange={(v) => {
+                    currentInput[input.key] = v;
                     props.onChangeInput(currentInput);
                 }} />
             })}
--- src/ventsim/view/controls/Control.js
+++ src/ventsim/view/controls/Control.js
@@ -1,26 +1,83 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import VariableName from './VariableName';
+
+
+function startDragging(initialValue, interval, range, mouseX, onChange, onCommit) {
+
+    var value = initialValue;
+
+    let fullDragSize = 300;
+    let pixels = fullDragSize / (((range[1] - range[0]) / interval));
+
+    let moveListener = (e) => {
+        let diff = e.clientX - mouseX;
+        if (Math.abs(diff) > pixels) {
+            value = value + Math.sign(diff) * interval * Math.round(Math.abs(diff) / pixels);
+            value = Math.max(range[0], Math.min(range[1], value));
+            onChange(value);
+            mouseX = e.clientX;
+        }
+    };
+
+    let leaveListener = (e) => {
+        window.removeEventListener('mousemove', moveListener);
+        window.removeEventListener('mouseup', leaveListener);
+        onCommit(value);
+    }
+
+    window.addEventListener('mousemove', moveListener);
+    window.addEventListener('mouseup', leaveListener);
+}
 
 function Control(props) {
 
+    let [tempVal, setTempVal] = useState(null);
+    let mouseX = useRef(null);
 
     let variable = props.variable;
     let mutable = props.mutable;
+    let value = tempVal === null ? props.value : tempVal;
+    let interval = variable.interval || 1;
 
+    var valid = true;
     var rotation;
     if (mutable) {
-        let percent = (props.value - variable.range[0]) / (variable.range[1] - variable.range[0]);
+        let percent = (value - variable.range[0]) / (variable.range[1] - variable.range[0]);
         percent = Math.max(0, Math.min(1, percent));
         rotation = -140 + (percent * 2 * 140);
+
+        valid = value <= variable.range[1] && value >= variable.range[0];
     }
 
-    return <div className="control">
+    let patientClass = (key) => (key.endsWith("_1") ? "patient-1" : (key.endsWith("_2") ? "patient-2" : ""));
+
+    return <div className={"control " + patientClass(variable.key)}>
         <div className="key"><VariableName variable={variable} /></div>
-        <div className="value">
-            <input type="text" value={props.value} disabled={!mutable} />
+        <div className={"value " + (valid ? "" : "invalid")}>
+            <input type="text" value={value || "0"} disabled={!mutable} onChange={(e) => {
+                setTempVal(e.target.value.replace(/[^\d.-]/g, ''));
+            }} onBlur={(e) => {
+                // intervals
+                value = variable.range[0] + interval * Math.round((value - variable.range[0]) / interval);
+
+                if (valid) {
+                    props.onChange(parseFloat(value));
+                } else if (value < variable.range[0]) { // min / max
+                    props.onChange(variable.range[0]);
+                } else if (value > variable.range[1]) {
+                    props.onChange(variable.range[1]);
+                }
+                setTempVal(null);
+            }}/>
         </div>
         <div className="unit">{variable.unit}</div>
-        { mutable ? <div className="dial">
+        { mutable ? <div className="dial"
+                onMouseDown={(e) => {
+                    startDragging(value, interval, variable.range, e.clientX, setTempVal, (v) => {
+                        props.onChange(v);
+                        setTempVal(null);
+                    });
+                }}>
             <div className="dial-control" style={{transform: "rotate(" + rotation + "deg)"}}></div>
             <div className="min">{variable.range[0]}</div>
             <div className="max">{variable.range[1]}</div>
